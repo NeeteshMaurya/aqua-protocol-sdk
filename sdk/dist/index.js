c78f142feb181648c3d5f3ada6ca91fa0ee75052
NeeteshMaurya <neetesh010798@gmail.com>
2023-06-09 10:32:56 +0530

    tsdx build remove

--- sdk/dist/index.js
+++ sdk/dist/index.js
@@ -2,7 +2,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./aqua-protocol-sdk.cjs.production.min.js')
+  module.exports = require('./sdk.cjs.production.min.js')
 } else {
-  module.exports = require('./aqua-protocol-sdk.cjs.development.js')
+  module.exports = require('./sdk.cjs.development.js')
 }
